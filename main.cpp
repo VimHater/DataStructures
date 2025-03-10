3412a4a1e92e22817e16ef5e0b3544347691d915
VimHater <24520826@gm.uit.edu.vn>
2025-03-10 10:16:56 +0700

    update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "Headers/Data_Structures.h"
 int main() {
     Linked_list<int> list;
-    list.append(10);
-    list.append(10);
-    list.print();
+    while (true) {
+        int n;
+        std::cin >> n;
+        list.append(n);
+        list.print();
+    }
 }
